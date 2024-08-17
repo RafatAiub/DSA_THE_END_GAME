35b1193abcd373f9c50321b319bade81250b7cc7
RafatAiub <]>
2024-08-17 12:32:29 +0600

    FizzBuzz updated

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -2,9 +2,9 @@
 
 const fizzBuzz = (n) => {
   for (let i = 1; i < n; i++) {
-    if (i % 3) console.log("Fizz");
-    if (i % 5) console.log("Buzz");
-    if (i % 5 && i % 3) console.log("FizzBuzz");
+    if (i % 3 === 0) console.log("Fizz");
+    if (i % 5 === 0) console.log("Buzz");
+    if (i % 5 === 0 && i % 3 === 0) console.log("FizzBuzz");
     else {
       console.log(i);
     }
